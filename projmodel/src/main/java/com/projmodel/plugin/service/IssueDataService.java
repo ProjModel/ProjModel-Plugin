c8eb713d183736576422e31eed410a08019226b7
ная <nastya034rus@yandex.ru>
2026-04-15 14:45:38 +0300

    feat[IssueDataService] Добавлено поле и метод в интерфейс

--- projmodel/src/main/java/com/projmodel/plugin/service/IssueDataService.java
+++ projmodel/src/main/java/com/projmodel/plugin/service/IssueDataService.java
@@ -1,7 +1,14 @@
 package com.projmodel.plugin.service;
 
+import com.atlassian.jira.issue.Issue;
+
+import java.util.List;
+
 /**
  * Интерфейс для реализации чтения данных из задач проектов Jira (само чтение)
  */
 public interface IssueDataService {
+
+    List<Issue> getIssuesForProject(String projectKey);
+    List<Issue> getOpenIssuesForProject(String projectKey);
 }
