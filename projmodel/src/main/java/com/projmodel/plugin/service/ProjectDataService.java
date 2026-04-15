52667f4ac6782bfd57e86bbf9593b8837b0d9aa0
ная <nastya034rus@yandex.ru>
2026-04-15 14:44:03 +0300

    feat[ProjectDataService] Добавлено поле и метод в интерфейс

--- projmodel/src/main/java/com/projmodel/plugin/service/ProjectDataService.java
+++ projmodel/src/main/java/com/projmodel/plugin/service/ProjectDataService.java
@@ -1,7 +1,14 @@
 package com.projmodel.plugin.service;
 
+import com.atlassian.jira.project.Project;
+
+import java.util.List;
+
 /**
  * Интерфейс для реализации чтения данных из проектов Jira (способность в целом читать данные; будет реализовано через Jira API)
  */
 public interface ProjectDataService {
+
+    List<Project> getAllProjects();
+    Project getProjectByKey(String projectKey);
 }
