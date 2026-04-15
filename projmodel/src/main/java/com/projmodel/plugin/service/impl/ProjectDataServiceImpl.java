b3eb499644aa15c9094e17099daaaeeecb65e5b6
ная <nastya034rus@yandex.ru>
2026-04-15 14:58:58 +0300

    feat[ProjectDataServiceImpl] Удалены заглушки в UnitTest файле, создан файл для имплементации интерфейса ProjectDataService

---
+++ projmodel/src/main/java/com/projmodel/plugin/service/impl/ProjectDataServiceImpl.java
@@ -0,0 +1,15 @@
+package com.projmodel.plugin.service.impl;
+
+import com.atlassian.plugin.spring.scanner.annotation.export.ExportAsService;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
+import com.atlassian.sal.api.ApplicationProperties;
+import com.projmodel.plugin.api.MyPluginComponent;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Named
+public class ProjectDataServiceImpl
+{
+       
+}
--- projmodel/src/test/java/ut/com/projmodel/plugin/MyComponentUnitTest.java
+++ projmodel/src/test/java/ut/com/projmodel/plugin/MyComponentUnitTest.java
@@ -1,17 +1,14 @@
 package ut.com.projmodel.plugin;
 
 import org.junit.Test;
-import com.projmodel.plugin.api.MyPluginComponent;
-import com.projmodel.plugin.service.impl.ProjectDataService;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class MyComponentUnitTest
 {
     @Test
     public void testMyName()
     {
-        MyPluginComponent component = new ProjectDataService(null);
-        assertEquals("names do not match!", "myComponent",component.getName());
+
     }
 }
