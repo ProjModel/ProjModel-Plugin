b8f9c8da2f743db498ca180250ae9336266f64d6
ная <nastya034rus@yandex.ru>
2026-04-15 14:59:51 +0300

    feat[ProjectDataServiceImpl] Имплементация интерфейса с заглушками

--- projmodel/src/main/java/com/projmodel/plugin/service/impl/ProjectDataServiceImpl.java
+++ projmodel/src/main/java/com/projmodel/plugin/service/impl/ProjectDataServiceImpl.java
@@ -1,15 +1,26 @@
 package com.projmodel.plugin.service.impl;
 
+import com.atlassian.jira.project.Project;
 import com.atlassian.plugin.spring.scanner.annotation.export.ExportAsService;
 import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
 import com.atlassian.sal.api.ApplicationProperties;
 import com.projmodel.plugin.api.MyPluginComponent;
+import com.projmodel.plugin.service.ProjectDataService;
 
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.util.List;
 
 @Named
-public class ProjectDataServiceImpl
+public class ProjectDataServiceImpl implements ProjectDataService
 {
-       
+    @Override
+    public List<Project> getAllProjects() {
+        return List.of();
+    }
+
+    @Override
+    public Project getProjectByKey(String projectKey) {
+        return null;
+    }
 }
