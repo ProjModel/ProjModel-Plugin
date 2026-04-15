eebb05b6bf9556d4f9a0af7973d13b56820a281e
ная <nastya034rus@yandex.ru>
2026-04-15 16:38:12 +0300

    test[DebugServlet] Добавлены поля и конструктор

--- projmodel/src/main/java/com/projmodel/plugin/web/DebugServlet.java
+++ projmodel/src/main/java/com/projmodel/plugin/web/DebugServlet.java
@@ -1,6 +1,22 @@
 package com.projmodel.plugin.web;
 
+import com.projmodel.plugin.service.IssueDataService;
+import com.projmodel.plugin.service.ProjectDataService;
+
+import javax.inject.Inject;
+import javax.inject.Named;
 import javax.servlet.http.HttpServlet;
 
+@Named
 public class DebugServlet extends HttpServlet {
+
+    private final ProjectDataService projectDataService;
+    private final IssueDataService issueDataService;
+
+    @Inject
+    public DebugServlet(ProjectDataService projectDataService,
+                        IssueDataService issueDataService) {
+        this.projectDataService = projectDataService;
+        this.issueDataService = issueDataService;
+    }
 }
