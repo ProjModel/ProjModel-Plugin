89d1323fa639082da80b10c85fbfb9c23d267f6b
ная <nastya034rus@yandex.ru>
2026-04-15 15:23:59 +0300

    feat[IssueDataServiceImpl] Добавлен приватный метод для обработки полученных данных о юзере и получения задач по jql

--- projmodel/src/main/java/com/projmodel/plugin/service/impl/IssueDataServiceImpl.java
+++ projmodel/src/main/java/com/projmodel/plugin/service/impl/IssueDataServiceImpl.java
@@ -1,14 +1,23 @@
 package com.projmodel.plugin.service.impl;
 
+import com.atlassian.jira.bc.issue.search.SearchService;
+import com.atlassian.jira.component.ComponentAccessor;
 import com.atlassian.jira.issue.Issue;
+import com.atlassian.jira.issue.search.SearchResults;
+import com.atlassian.jira.user.ApplicationUser;
 import com.projmodel.plugin.service.IssueDataService;
 
 import javax.inject.Named;
+import javax.management.Query;
+import javax.naming.directory.SearchResult;
+import java.util.Collections;
 import java.util.List;
 
 @Named
 public class IssueDataServiceImpl implements IssueDataService {
-    
+
+    private final SearchService _searchService;
+
     @Override
     public List<Issue> getIssuesForProject(String projectKey) {
         return List.of();
@@ -17,5 +26,27 @@ public class IssueDataServiceImpl implements IssueDataService {
     @Override
     public List<Issue> getOpenIssuesForProject(String projectKey) {
         return List.of();
+    }
+
+    private List<Issue> searchIssuesByJql(String jql) {
+        try {
+            ApplicationUser user = ComponentAccessor.getJiraAuthenticationContext().getLoggedInUser();
+            if(user == null) {
+                return Collections.emptyList();
+            }
+
+            SearchService.ParseResult parseResult = _searchService.parseQuery(user, jql);
+            if(!parseResult.isValid()) {
+                return Collections.emptyList();
+            }
+
+            Query q = parseResult.getQuery();
+            SearchResults<Issue> results = _searchService.search(user, q, com.atlassian.jira.web.bean.PagerFilter.getUnlimitedFilter());
+
+            return results.getResults();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Collections.emptyList();
+        }
     }
 }
