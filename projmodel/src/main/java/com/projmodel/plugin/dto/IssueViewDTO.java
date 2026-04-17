042b6c217186b0949d8d21366ca67e38786958f7
ная <nastya034rus@yandex.ru>
2026-04-17 12:26:42 +0300

    feat[IssueViewDTO] Добавлены поля и реализован конструктор

--- projmodel/src/main/java/com/projmodel/plugin/dto/IssueViewDTO.java
+++ projmodel/src/main/java/com/projmodel/plugin/dto/IssueViewDTO.java
@@ -1,7 +1,23 @@
 package com.projmodel.plugin.dto;
 
+import java.util.Date;
+
 /**
  * DTO для отображения главных данных о задаче у пользователя
  */
 public class IssueViewDTO {
+
+    private String key;
+    private String summary;
+    private String status;
+    private String assignee;
+    private Date dueDate;
+
+    public IssueViewDTO(String key, String summary, String status, String assignee, Date dueDate) {
+        this.key = key;
+        this.summary = summary;
+        this.status = status;
+        this.assignee = assignee;
+        this.dueDate = dueDate;
+    }
 }
