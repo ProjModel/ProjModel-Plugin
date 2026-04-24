1bd7467a630a00eb04d047a14ed8b6b76864d504
Grechishnikov Ivan <genshin1mp@mail.ru>
2026-04-24 08:36:47 +0300

    feat[ReportServiceImpl] Добавлены необходимые зависимости в файл реализации сервиса экспорта

--- projmodel/src/main/java/com/projmodel/plugin/service/impl/ReportServiceImpl.java
+++ projmodel/src/main/java/com/projmodel/plugin/service/impl/ReportServiceImpl.java
@@ -1,4 +1,17 @@
 package com.projmodel.plugin.service.impl;
 
+import com.atlassian.activeobjects.external.ActiveObjects;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
+import com.projmodel.plugin.ao.ReportTaskAO;
+import com.projmodel.plugin.dto.IssueViewDTO;
+import com.projmodel.plugin.report.ReportGenerator;
+import com.projmodel.plugin.service.IssueDataService;
+import com.projmodel.plugin.service.ReportService;
+import net.java.ao.Query;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.util.*;
+
 public class ReportServiceImpl {
 }
