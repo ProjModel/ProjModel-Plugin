90e8ffe3039a6ea734038627eae0ba9f01608de6
Grechishnikov Ivan <genshin1mp@mail.ru>
2026-04-24 04:43:51 +0300

    feat[WorkloadAnalysisServiceImpl] Добавлены сервис Jira и конструктор для класса

--- projmodel/src/main/java/com/projmodel/plugin/service/impl/WorkloadAnalysisServiceImpl.java
+++ projmodel/src/main/java/com/projmodel/plugin/service/impl/WorkloadAnalysisServiceImpl.java
@@ -14,6 +14,21 @@ import java.util.stream.Collectors;
  * Сервис по анализу загрузки участников команды на основе незавершенных задач проекта
  */
 public class WorkloadAnalysisServiceImpl implements WorkloadAnalysisService {
+
+    /**
+     * Сервис для получения данных о задачах из Jira
+     */
+    private final IssueDataService _issueDataService;
+
+    /**
+     * Конструктор сервиса
+     * @param issueDataService сервис по работе с задачами
+     */
+    @Inject
+    public WorkloadAnalysisServiceImpl(IssueDataService issueDataService) {
+        _issueDataService = issueDataService;
+    }
+
     @Override
     public List<WorkloadViewDTO> analyzeWorkload(String projectKey) {
         return List.of();
