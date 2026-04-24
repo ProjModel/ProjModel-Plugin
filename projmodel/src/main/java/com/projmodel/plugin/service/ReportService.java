50a0562a9f15620a981cc37dbdbce5d02d164fa2
Grechishnikov Ivan <genshin1mp@mail.ru>
2026-04-24 08:34:40 +0300

    feat[ReportService] Добавлены необходимые зависимости в интерфейс экспорта

--- projmodel/src/main/java/com/projmodel/plugin/service/ReportService.java
+++ projmodel/src/main/java/com/projmodel/plugin/service/ReportService.java
@@ -1,4 +1,12 @@
 package com.projmodel.plugin.service;
 
+import com.projmodel.plugin.ao.ReportTaskAO;
+import java.util.List;
+
+/**
+ * Интерфейс сервиса для работы с отчётами
+ * Предоставляет методы для создания запросов на отчёты и их генерации
+ */
+
 public interface ReportService {
 }
