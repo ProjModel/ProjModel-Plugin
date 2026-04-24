fbf1e52e5af1a913888a0c1075426ee657eb967a
Grechishnikov Ivan <genshin1mp@mail.ru>
2026-04-24 04:42:17 +0300

    feat[WorkloadAnalysisServiceImpl] Добавлены необходимые include и реализация интерфейса WorkloadAnalysisService

--- projmodel/src/main/java/com/projmodel/plugin/service/impl/WorkloadAnalysisServiceImpl.java
+++ projmodel/src/main/java/com/projmodel/plugin/service/impl/WorkloadAnalysisServiceImpl.java
@@ -1,4 +1,21 @@
 package com.projmodel.plugin.service.impl;
 
-public class WorkloadAnalysisServiceImpl {
+import com.projmodel.plugin.dto.IssueViewDTO;
+import com.projmodel.plugin.dto.WorkloadViewDTO;
+import com.projmodel.plugin.service.IssueDataService;
+import com.projmodel.plugin.service.WorkloadAnalysisService;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * Сервис по анализу загрузки участников команды на основе незавершенных задач проекта
+ */
+public class WorkloadAnalysisServiceImpl implements WorkloadAnalysisService {
+    @Override
+    public List<WorkloadViewDTO> analyzeWorkload(String projectKey) {
+        return List.of();
+    }
 }
