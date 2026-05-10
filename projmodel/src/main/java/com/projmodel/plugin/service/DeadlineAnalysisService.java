bd7056926647ab867df5dd84e0be597e10f4bf39
Pomorov Artyom <pomorov_artem@bk.ru>
2026-05-10 23:01:04 +0300

    feat[Service] Реализован интерфейс DeadlineAnalysisService

--- projmodel/src/main/java/com/projmodel/plugin/service/DeadlineAnalysisService.java
+++ projmodel/src/main/java/com/projmodel/plugin/service/DeadlineAnalysisService.java
@@ -1,4 +1,9 @@
 package com.projmodel.plugin.service;
 
+import com.projmodel.plugin.dto.DeadlineIssueDTO;
+
+import java.util.List;
+
 public interface DeadlineAnalysisService {
+    List<DeadlineIssueDTO> analyzeDeadlines(String projectKey);
 }
